34180250b064af271ebceef30e125c336ea494fa
Evandro Paulo Folletto <evandrofolletto@gmail.com>
2022-06-15 20:35:05 -0300

    rodape finalizado

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,20 +1,89 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import instagram from "./../img/instagram.png";
+import facebook from "./../img/facebook.png";
+import twitter from "./../img/twitter.png";
+import youtube from "./../img/youtube.png";
 
 const Rodape = styled.div`
   display: flex;
   background-color: orange;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  height: 60px;
   grid-column: 1/-1;
+`
+
+const ImagemRedeSocial = styled.img`
+  width: 25px;
+  margin: 0 10px 0 5px;
+`
+
+const TextoLogoGeral = styled.h4`
+  display: flex;
+  align-items: center;
+  @import url('https://fonts.googleapis.com/css2?family=Comfortaa&display=swap');
+  font-family: 'Comfortaa', cursive;
+  margin: 0 0 0 20px;
+`
+
+const TextoLogo1 = styled.h4`
+  background-color: red;
+  padding: 5px 0 5px 5px;
+  border-radius: 10px;
+`
+
+const TextoLogo2 = styled.h4`
+  margin: 0 0 0 2px;
+`
+
+const TextoCentral = styled.h4`
+`
+
+const SigamNos = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 20px 3px 0;
+`
+
+const Redes = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0;
+`
+
+const TextoSigam = styled.p`
+  text-left: center;
+  margin: 0 0 3px 0;
 `
 
 export class Footer extends Component {
 	render() {
 		return (
       <Rodape>
-        RODAPÉ
+
+        <TextoLogoGeral>
+          <TextoLogo1>
+            shirtX:
+          </TextoLogo1>
+          <TextoLogo2>
+            camisas leves que te fazem voar
+          </TextoLogo2>
+        </TextoLogoGeral>
+
+        <TextoCentral>
+          Pais Leme, 215, Pinheiros. CEP: 05424-150
+        </TextoCentral>
+
+        <SigamNos>
+          <TextoSigam>Sigam-nos nas redes sociais:</TextoSigam>
+          <Redes>
+            <ImagemRedeSocial src={instagram} />
+            <ImagemRedeSocial src={youtube} />
+            <ImagemRedeSocial src={twitter} />
+            <ImagemRedeSocial src={facebook} />
+          </Redes>
+        </SigamNos>
+
       </Rodape>
     )
 	}
