a67ff4e5ca2ee9b4a3921a1c4a919aaf5e7e3938
Evandro Paulo Folletto <evandrofolletto@gmail.com>
2022-06-15 23:24:22 -0300

    cabecalho adicionado

--- src/components/carrinho/Carrinho.js
+++ src/components/carrinho/Carrinho.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const MenuDireita = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: pink;
-  padding: 10px 10px 10px 20px;
+
 `
 
 export class Carrinho extends Component {
